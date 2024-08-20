21ecfb512856d9370c36df23ad59ddc81ae99a63
Your Name Goes Here <username@email.com>
2024-08-20 16:50:55 +0700

    after class

--- student/src/app/app-routing.module.ts
+++ student/src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './student/student.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'student', component: StudentComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'sing-up', component: SingUpComponent }
 ];
 
 @NgModule({
